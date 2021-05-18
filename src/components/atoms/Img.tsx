3bf8f6aa9a7b93b454f02f365ac1f573d4ceae40
KituneUdon <kituneudon97@gmail.com>
2021-05-18 17:51:40 +0900

    #3 画像のコンポーネントが親コンポーネントより大きくなることを防いた

--- src/components/atoms/Img.tsx
+++ src/components/atoms/Img.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 type Props = {
   src: string;
   alt: string;
 };
 
-const Img: FC<Props> = ({ src, alt }) => <img src={src} alt={alt} />;
+const style = css({
+  maxWidth: '100%',
+});
+
+const Img: FC<Props> = ({ src, alt }) => (
+  <img css={style} src={src} alt={alt} />
+);
 
 export default Img;
