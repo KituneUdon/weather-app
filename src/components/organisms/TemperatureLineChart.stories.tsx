5168d9363a88a84c522667092ee56e2116cee012
KituneUdon <kituneudon97@gmail.com>
2021-05-22 11:49:10 +0900

    #9 storyを修正

--- src/components/organisms/TemperatureLineChart.stories.tsx
+++ src/components/organisms/TemperatureLineChart.stories.tsx
@@ -1,10 +1,21 @@
-// import { FC } from 'react';
+import { FC } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import TemperatureLineChart from './TemperatureLineChart';
+
+const queryClient = new QueryClient();
 
 export default {
   component: TemperatureLineChart,
   title: 'TemperatureLineChart',
+  decorators: [
+    // eslint-disable-next-line
+    (story: () => JSX.Element) => (
+      <QueryClientProvider client={queryClient}>{story()}</QueryClientProvider>
+    ),
+  ],
 };
 
-// export const Default: FC = () => <TemperatureLineChart />;
+export const Default: FC = () => (
+  <TemperatureLineChart location={{ latitude: 35, longitude: 139 }} />
+);
