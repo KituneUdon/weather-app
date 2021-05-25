c7a3ecde7852eba84d42a03b94035acf9bf0f6d8
KituneUdon <kituneudon97@gmail.com>
2021-05-25 11:35:51 +0900

    GitHub action setup

--- src/components/organisms/CurrentLocation.stories.tsx
+++ src/components/organisms/CurrentLocation.stories.tsx
@@ -9,8 +9,7 @@ export default {
   component: CurrentLocation,
   title: 'organisms/CurrentLocation',
   decorators: [
-    // eslint-disable-next-line
-    (story: () => JSX.Element) => (
+    (story: () => JSX.Element): JSX.Element => (
       <QueryClientProvider client={queryClient}>{story()}</QueryClientProvider>
     ),
   ],
